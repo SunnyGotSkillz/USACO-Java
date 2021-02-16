1a4c56cc8ee423a13286134ad6f161630c06a970
Sunny Vinay <sunnyvinay7@gmail.com>
2021-02-16 11:37:01 -0800

    Change Moocast Silver solution

--- USACO_Silver/Moocast.java
+++ USACO_Silver/Moocast.java
@@ -2,9 +2,9 @@
 PROBLEM: Moocast (USACO December 2016 Silver #3)
 LINK: http://www.usaco.org/index.php?page=viewproblem2&cpid=668
 
-First, find which cows can talk to which cows. I used a Cow class and array to achieve this. Then create an adjacency list that will represent a directed
-graph. Run DFS on the graph for each cow. We have to reset the values in the visited array because this is not running DFS on each connected component. We
-want to traverse the graph starting at each cow no matter what. After each traversal we store the max number cows that were reached.
+First, find which cows can talk to which cows using the distance formula between 2 coordinates and the power of the walkie-talkies. Then create an adjacency list 
+that will represent a directed graph. Run DFS on the graph for each cow. We have to reset the values in the visited array because this is not running DFS on each 
+connected component. We want to traverse the graph starting at each cow no matter what. After each traversal we store the max number cows that were reached.
 */
 
 import java.util.*;
@@ -12,74 +12,63 @@ import java.io.*;
 import java.lang.*;
 
 public class Main {
+    static int n;
+    static int[] x;
+    static int[] y;
+    static int[] p;
     static ArrayList<Integer>[] adj;
-    static boolean[] vis;
-    static int max = -1;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new FileReader("moocast.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("moocast.out")));
 
-        int n = Integer.parseInt(in.readLine());
-        Cow[] cows = new Cow[n];
+        n = Integer.parseInt(in.readLine());
+        x = new int[n];
+        y = new int[n];
+        p = new int[n];
         adj = new ArrayList[n];
-        vis = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-        }
-
+        visited = new boolean[n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(in.readLine());
-            cows[i] = new Cow(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            adj[i] = new ArrayList<>();
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
-                double dist = distance(cows[i], cows[j]);
-                if (dist <= cows[i].r) {
+                double dist = distance(x[i], x[j], y[i], y[j]);
+                if (p[i] >= dist) {
                     adj[i].add(j);
                 }
-                if (dist <= cows[j].r) {
+                if (p[j] >= dist) {
                     adj[j].add(i);
                 }
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(vis, false);
-            max = Math.max(max, dfs(i));
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dfs(i));
+            Arrays.fill(visited, false);
         }
 
-        out.println(max);
+        out.println(ans);
         out.close();
     }
 
-    static double distance(Cow a, Cow b) {
-        return Math.sqrt(((a.x-b.x)*(a.x-b.x)) + ((a.y-b.y)*(a.y-b.y)));
+    public static double distance(int x1, int x2, int y1, int y2) {
+        return Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
     }
 
-    static class Cow {
-        int x;
-        int y;
-        int r;
-        public Cow(int x, int y, int r) {
-            this.x = x;
-            this.y = y;
-            this.r = r;
-        }
-    }
-
-    static int dfs(int node) {
-        if(vis[node]) {
-            return 0;
-        }
+    public static int dfs(int node) {
         int visCount = 1;
-        vis[node] = true;
+        visited[node] = true;
         for(int next : adj[node]) { // goes through each of the current node's neighbors
-            if(!vis[next]) {
+            if(!visited[next]) {
                 visCount += dfs(next);
-                //dfs(next);
             }
         }
 
