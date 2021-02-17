b961471d5a11d9bb18f1e8403456526c5bf0f947
Sunny Vinay <sunnyvinay7@gmail.com>
2021-02-17 11:55:19 -0800

    Update Mootube Silver solution

--- USACO_Silver/Mootube.java
+++ USACO_Silver/Mootube.java
@@ -3,7 +3,8 @@ PROBLEM: Mootube (USACO January 2018 Silver #3)
 LINK: http://www.usaco.org/index.php?page=viewproblem2&cpid=788
 
 This problem can be solved using DSU or DFS with an undirected, weighted graph. We do DFS on each question. We never want to traverse an edge with edge weight 
-strictly less than k, so we ignore those edges. We can then count how many other vertices we have visited.
+strictly less than k, so we ignore those edges. We can then count how many other vertices match our relevancy constraints. We do this by having a global static 
+variable called ans and resetting and printing its value after every DFS for each of FJ's questions.
 */
 
 import java.util.*;
@@ -11,10 +12,11 @@ import java.io.*;
 import java.lang.*;
 
 public class Main {
+    static int n;
+    static int q;
     static ArrayList<Edge>[] adj;
-    static int n, q;
     static boolean[] visited;
-    static int previous = Integer.MAX_VALUE;
+    static int ans = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new FileReader("mootube.in"));
@@ -26,51 +28,47 @@ public class Main {
         adj = new ArrayList[n];
         visited = new boolean[n];
         for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Edge>();
-        }
-        for (int i = 0; i < n-1; i++) {
-            st = new StringTokenizer(in.readLine());
-            int a = Integer.parseInt(st.nextToken())-1;
-            int b = Integer.parseInt(st.nextToken())-1;
-            int r = Integer.parseInt(st.nextToken());
-            adj[a].add(new Edge(b, r));
-            adj[b].add(new Edge(a, r));
+            adj[i] = new ArrayList<>();
         }
 
-        for (int i = 1; i <= q; i++) {
-            Arrays.fill(visited, false);
-            previous = Integer.MAX_VALUE;
+        for (int i = 0; i < n-1; i++) {
+            st = new StringTokenizer(in.readLine());
+            int pi = Integer.parseInt(st.nextToken()) - 1;
+            int qi = Integer.parseInt(st.nextToken()) - 1;
+            int ri = Integer.parseInt(st.nextToken());
+            adj[pi].add(new Edge(qi, ri));
+            adj[qi].add(new Edge(pi, ri));
+        }
+
+        for (int i = 0; i < q; i++) {
             st = new StringTokenizer(in.readLine());
             int k = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int ans = dfs(v-1, k);
-            out.println(ans-1);
+            int v = Integer.parseInt(st.nextToken()) - 1;
+            dfs(v, k);
+            out.println(ans);
+            Arrays.fill(visited, false);
+            ans = 0;
         }
 
         out.close();
     }
 
-    static class Edge {
-        int to;
-        int weight;
-
-        public Edge(int to, int weight){
-            this.to = to;
-            this.weight = weight;
+    public static void dfs(int node, int k) {
+        visited[node] = true;
+        for(Edge next : adj[node]) {
+            if(!visited[next.to] && next.r >= k) {
+                dfs(next.to, k);
+            }
         }
     }
 
-    static int dfs(int node, int k) {
-        int visCount = 1;
-        visited[node] = true;
-        for (Edge next : adj[node]) { // goes through each of the current node's neighbors
-            int temp = Math.min(previous, next.weight);
-            if (!visited[next.to] && temp >= k) {
-                previous = temp;
-                visCount += dfs(next.to, k);
-            }
+    public static class Edge {
+        int to;
+        int r;
+
+        public Edge(int to, int r){
+            this.to = to;
+            this.r = r;
         }
-        
-        return visCount;
     }
 }
